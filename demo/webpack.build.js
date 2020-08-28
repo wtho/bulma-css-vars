7b474435ac9d578b6615824c427679b86839be2f
Thomas Wirth <thomas@wtho.de>
2020-08-28 18:22:03 +0200

    fix: compatibility with bulma v0.9.2
    also:
    * upgraded and updated dependencies

--- demo/webpack.build.js
+++ demo/webpack.build.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 

e3a0ad68a38b6a860524ce1a8bc10823f668fb57
Suneesh S K <suneeshsk3@gmail.com>
2022-08-10 20:58:02 +0530

    with darkmode

--- bin/bulma-css-vars.js
+++ bin/bulma-css-vars.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
-const { runCli, runCliInit } = require('../lib/cli.js')
+const { runCli, runCliDarkMode, runCliInit } = require('../lib/cli.js')
 
 const cwd = process.cwd()
 
 const init = process.argv.indexOf('--init') !== -1
+const darkMode = process.argv.indexOf('--darkmode') !== -1
 
 if (init) {
   runCliInit(cwd).then(() => runCli(cwd))
 } else {
   runCli(cwd)
+  if (darkMode) {
+    runCliDarkMode(cwd)
+  }
 }

3514a2b6249fef8625e2a67b4b7a98f8804372d2
Suneesh S K <suneeshsk3@gmail.com>
2022-08-12 23:36:36 +0530

    fix load path sass

--- src/compile-sass.ts
+++ src/compile-sass.ts
@@ -1,6 +1,8 @@
 import * as sass from 'sass'
 
 export function compileSass(sassFilePath: string) {
-  const compileResult = sass.compile(sassFilePath)
+  const compileResult = sass.compile(sassFilePath, {
+    loadPath: ['./node_modules'],
+  })
   return compileResult.css
 }
